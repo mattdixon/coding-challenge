3835662554f3702e3efd2e1653700967ef8ff4c3
Matt Dixon <matt.dixon@frontrangesystems.com>
2021-01-11 18:22:06 -0700

    initial checkin

---
+++ csharp/CodingChallenge.Tests/MyTests.cs
@@ -0,0 +1,22 @@
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CodingChallenge.Tests
+{
+    public class MyTests
+    {
+        // the country search string
+        private string SearchString => "u";
+        private MyTestService ItemUnderTest { get; set; }
+
+        [SetUp]
+        public void Setup() { }
+
+        [Test]
+        public async Task AsyncTest()
+        {
+            var json = await ItemUnderTest.GetCountriesJsonAsync("u");
+            Assert.IsNotEmpty(json);
+        }
+    }
+}
---
+++ csharp/CodingChallenge/MyTestService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace CodingChallenge
+{
+    public class MyTestService
+    {
+        public async Task<string> GetCountriesJsonAsync(string name)
+        {
+            using var client = CreateClient();
+            var url = $"https://restcountries.eu/rest/v2/name/{name}";
+            var response = await client.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException("Unknown Error");
+            }
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        private HttpClient CreateClient()
+        {
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+    }
+}
